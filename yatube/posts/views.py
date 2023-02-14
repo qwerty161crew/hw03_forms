763a334e3d31ab50412ef2468637546575dd61cc
Алексей Матвеев <qwerty161crew@gmail.com>
2023-02-14 12:10:33 +0300

    добавил паджинатор

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -22,9 +22,13 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).all()[:10]
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
         'posts': posts,
+        'page_obj': page_obj
     }
     return render(request, 'posts/group_list.html', context)
 
