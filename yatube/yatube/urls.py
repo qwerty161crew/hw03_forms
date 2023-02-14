447cb2383faef31c0ec21338b4acb7d0626e9577
Алексей Матвеев <qwerty161crew@gmail.com>
2023-02-14 13:14:36 +0300

    исправил головной url

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
     path('about/', include('about.urls', namespace='about')),
-    path('profile/', include('posts.urls', namespace='profile')),
+    path('profile/<str:username>.',
+         include('posts.urls', namespace='profile')),
     path('auth/', include('users.urls', namespace='auth')),
     path('auth/', include('django.contrib.auth.urls')),
 ]
