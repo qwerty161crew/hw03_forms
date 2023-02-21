97a1719c37332ec06940b09772b1472127374a1f
Алексей Матвеев <qwerty161crew@gmail.com>
2023-02-21 13:20:43 +0300

    добавил локализацию модели

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,29 +1,38 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(_('title'), max_length=200)
+    slug = models.SlugField(_('slug'), unique=True)
+    description = models.TextField(_('description'))
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = _('Group')
+        verbose_name_plural = _('Groups')
+
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(_('text'))
+    pub_date = models.DateTimeField(_('pub_date'), auto_now_add=True)
     author = models.ForeignKey(
+        _('author'),
         User,
         on_delete=models.CASCADE,
         related_name='posts'
     )
     group = models.ForeignKey(
+        _('group'),
         Group, on_delete=models.SET_NULL,
         related_name='posts', blank=True, null=True)
 
     class Meta:
         ordering = ('-pub_date',)
+        verbose_name = _('Post')
+        verbose_name_plural = _('Posts')
