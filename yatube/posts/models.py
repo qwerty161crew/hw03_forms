75bca83e98a5e1938bbc6086db10a98e184f649e
Алексей Матвеев <qwerty161crew@gmail.com>
2023-02-22 18:52:55 +0300

    добавил переводы моделям

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(_('заголовок'), max_length=200)
-    slug = models.SlugField(_('слаг'), unique=True)
+    slug = models.SlugField(_('адрес'), unique=True)
     description = models.TextField(_('описание'))
 
     def __str__(self):
@@ -24,11 +24,12 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='автор'
     )
     group = models.ForeignKey(
         Group, on_delete=models.SET_NULL,
-        related_name='posts', blank=True, null=True)
+        related_name='posts', blank=True, null=True, verbose_name='группа')
 
     class Meta:
         ordering = ('-pub_date',)
